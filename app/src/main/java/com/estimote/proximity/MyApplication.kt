5b9f83cb1ecccd376f5d3632407846c36de633bc
liperium <mattysgervais@gmail.com>
2022-05-24 16:09:19 -0400

    Initial commit

---
+++ app/src/main/java/com/estimote/proximity/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.estimote.proximity
+
+import android.app.Application
+import com.estimote.proximity_sdk.api.EstimoteCloudCredentials
+
+//
+// Running into any issues? Drop us an email to: contact@estimote.com
+//
+
+class MyApplication : Application() {
+
+    val cloudCredentials =  EstimoteCloudCredentials("mattys-gervais1-uqac-ca-s--033", "c71b4c5ef5517e9afe7db1f86eaf0b7c")
+}
