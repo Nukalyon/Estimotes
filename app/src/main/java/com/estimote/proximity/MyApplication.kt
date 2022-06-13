5e8e31e99ac65963e11410b548502d172827036f
liperium <mattysgervais@gmail.com>
2022-06-13 13:43:45 -0400

    Added account

--- app/src/main/java/com/estimote/proximity/MyApplication.kt
+++ app/src/main/java/com/estimote/proximity/MyApplication.kt
@@ -6,8 +6,10 @@ import com.estimote.proximity_sdk.api.EstimoteCloudCredentials
 //
 // Running into any issues? Drop us an email to: contact@estimote.com
 //
+//  Login
+//  mattys.gervais1@uqac.ca
+//  uqacisthebest2022@
 
 class MyApplication : Application() {
-
     val cloudCredentials =  EstimoteCloudCredentials("mattys-gervais1-uqac-ca-s--033", "c71b4c5ef5517e9afe7db1f86eaf0b7c")
 }
